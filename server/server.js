0c8f06f4c8073663e57ae4f4d82d60573564b2ac
Qazal <jalilian.gh@gmail.com>
2018-09-20 14:40:11 +0430

    POST users route and token added

--- server/server.js
+++ server/server.js
@@ -80,6 +80,28 @@ app.patch('/todos/:id', (req, res) => {
 });
 
 
+app.post('/users', (req, res) => {
+    var user = new User({
+        email: req.body.email,
+        password: req.body.password
+    });
+
+    User.findByToken
+
+
+    user.save().then(() => {
+        return user.generateAuthToken();
+
+    }).then((token) => {
+        res.header('x-auth',token).send(user);
+    }).catch((e) => {
+        res.status(400).send(e);
+    });
+});
+
+
+
+
 
 
 
