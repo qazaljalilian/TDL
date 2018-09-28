267af89601b4b60b135d5e92b463b560cb2963d2
Qazal <jalilian.gh@gmail.com>
2018-09-28 15:56:54 +0330

    login and home route added

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,20 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { Routes, RouterModule } from "@angular/router";
 import { AppComponent } from './app.component';
 import { HttpService } from './http.service';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { AuthGuard } from './auth-guard.service';
+import { AuthService } from './auth.service';
 
+
+const appRoutes: Routes = [ 
+  {path:'', canActivate:[AuthGuard],component:HomeComponent},
+  { path: 'login', component: LoginComponent },
+];
 @NgModule({
   declarations: [
-    AppComponent
+    LoginComponent,
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [HttpService],
+  providers: [HttpService, AuthService,AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ client/src/app/auth.service.ts
@@ -0,0 +1,28 @@
+import { promise } from "selenium-webdriver";
+
+export class AuthService{
+    loggedIn= true;
+
+isAuthenticate(){
+    const promise= new Promise(
+        (resolve,reject)=>{
+            setTimeout(() => {
+                resolve(this.loggedIn);
+            }, 800);
+        });
+        return promise;
+}
+
+
+    login (){
+        this.loggedIn=true;
+    }
+    logout(){
+        this.loggedIn= false;
+    }
+
+
+
+}
+
+
